75944e5e1e1333d67d7da093427798e3d39dd281
Michael Serrato <programador-experto97@hotmail.com>
2016-06-02 20:02:36 -0500

    [SEO] Actualización del Site Map #1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,50 +14,40 @@
 Route::group(['as' => 'site.'], function () {
 
   Route::group(['as' => 'welcome.'], function () {
-
     Route::get('/', [
       'uses'  => 'Welcome@index',
       'as'    => 'index'
     ]);
-
   });
 
   Route::group(['prefix' => 'contact', 'as' => 'contact.'], function () {
-
     Route::post('/send', [
       'uses'  => 'Contact@send',
       'as'    => 'send'
     ]);
-
     Route::post('/save', [
       'uses'  => 'Contact@save',
       'as'    => 'save'
     ]);
-
   });
 
   Route::group(['prefix' => 'calculator', 'as' => 'calculator.'], function () {
-
     Route::get('/', [
       'uses'  => 'Calculator@index',
       'as'    => 'index'
     ]);
-
     Route::get('/index', [
       'uses'  => 'Calculator@index',
       'as'    => 'index'
     ]);
-
     Route::match(['get', 'post'], '/features', [
         'uses'  => 'Calculator@features',
         'as'    => 'features'
     ]);
-
     Route::post('/send', [
       'uses'  => 'Calculator@send',
       'as'    => 'send'
     ]);
-
   });
 
 });
