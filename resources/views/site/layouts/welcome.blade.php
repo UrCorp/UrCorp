3c25c42a535b8504a0d5a0976ae781fcc969b97a
Michael Serrato <programador-experto97@hotmail.com>
2016-06-29 13:25:18 -0500

    [Mailing] Cambio de correo principal #7

--- resources/views/site/layouts/welcome.blade.php
+++ resources/views/site/layouts/welcome.blade.php
@@ -128,8 +128,8 @@
   <script type="text/javascript" src="{{ asset('public/assets/js/jquery.scrollspy.js') }}"></script>
   <script type="text/javascript" src="{{ asset('public/assets/js/jquery.anchor.js') }}"></script>
   <script type="text/javascript" src="{{ asset('public/assets/js/app/generic.js') }}"></script>
-  @if (!empty($controller) && !empty($view) && File::exists('public/assets/js/app/'.$controller.'.'.$view.'.js'))
-    <script type="text/javascript" src="{{ asset('public/assets/js/app/'.$controller.'.'.$view.'.js') }}"></script>
+  @if (!empty($controller) && !empty($view) && File::exists('public/assets/js/app/'.cstrtolower($controller).'.'.cstrtolower($view).'.js'))
+    <script type="text/javascript" src="{{ asset('public/assets/js/app/'.cstrtolower($controller).'.'.cstrtolower($view).'.js') }}"></script>
   @endif
 
 <script type="text/javascript">
