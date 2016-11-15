09dde1bcf373875743db56cf0c401e7e1d9c10f6
Michael Serrato <programador-experto97@hotmail.com>
2016-11-15 16:03:09 -0600

    [FrontEnd::Calculator] UrCorp logo link added.

--- resources/views/site/layouts/calculator.blade.php
+++ resources/views/site/layouts/calculator.blade.php
@@ -68,7 +68,9 @@
   <div id="header" class="col-xs-12 app-header no-side-padding no-height">
     <div id="navbar" class="app-navbar-fixed col-xs-12 no-side-padding">
       <div class="col-sm-3 col-xs-12 app-logo-container no-side-padding">
-        <img src="{!! asset('public/assets/img/v2/urcorp-logo.svg') !!}" alt="UrCorp logo" class="app-logo" title="UrCorp logo" />
+        <a href="{!! URL::to('/') !!}">
+          <img src="{!! asset('public/assets/img/v2/urcorp-logo.svg') !!}" alt="UrCorp logo" class="app-logo" title="UrCorp logo" />
+        </a>
         <!--<button id="btn-open-nav" class="btn-menu btn btn-primary pull-right visible-xs" type="button" data-toggle="collapse" data-target="#nav" aria-expanded="false" aria-controls="navbar">
             <span class="fa fa-bars"></span>
         </button>-->
