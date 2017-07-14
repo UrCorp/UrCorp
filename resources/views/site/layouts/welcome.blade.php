459672174dc600b10b7732681c4a8fa2b68e2656
Eduardo Vera <eduardo.vera.pineda@gmail.com>
2017-07-14 14:25:35 -0500

    [Backend] Facebook-Pixel code added to head tag

--- resources/views/site/layouts/welcome.blade.php
+++ resources/views/site/layouts/welcome.blade.php
@@ -1,15 +1,3 @@
-<!--
-  00000     00000 000 000 0000  0000000
-  000000   000000 000 000 000   000
-  000 000 000 000 000 000000    000000
-  000   000   000 000 000 000   000
-  000         000 000 000 00000 0000000
-
-  ======= [Software Developer] ========
-
-  E-mail: mikebsg01@gmail.com
-  Author: Michael Serrato
--->
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -50,6 +38,22 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/jquery.loading.css?v='.time()) }}">
   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/style.css?v='.time()) }}">
   <!-- END [Cascade Style Sheet Files] -->
+  <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '114390439150734'); // Insert your pixel ID here.
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=114390439150734&ev=PageView&noscript=1"
+    /></noscript>
+  <!-- DO NOT MODIFY -->
+  <!-- End Facebook Pixel Code -->                        
+
 </head>
 <body data-origin="{!! URL::to('/') !!}">
   <header>
