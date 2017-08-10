7871d8c97be8b542d293a0ed5d994a64125d28bc
Eduardo Vera <eduardo.vera.pineda@gmail.com>
2017-08-10 13:25:09 -0500

    [FrontEnd:Contest] the mail views for movil -> fixed

--- resources/views/site/emails/contest-client.blade.php
+++ resources/views/site/emails/contest-client.blade.php
@@ -1,9 +1,9 @@
 <?php
-  /*
+/*
   $contact['name']="Eduardo Vera - Web Developer";
   $contact['phone']="442·377·35·81";
   $contact['email']="eduardo.vera.pineda@gmail.com";
-  */
+*/
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,8 +12,8 @@
   <title>Contest UrCorp</title>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800,400italic,300italic' rel='stylesheet' type='text/css'>
 </head>
-<body style="font-family: 'Open sans',helvetica, arial, sans-serif;margin:0;background-color: #F5F5F5; color: #909090">
-<div style="display:block;margin:auto;width:640px;background-color:#FFFFFF;">
+<body style="background-color: #F5F5F5;color: #79888e">
+<div style="display:block;width:80%;margin:auto;margin-top:50px;margin-bottom:15px;background-color:white;padding:15px;border-radius:3px;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);">
   <div style="width:92%;display:block;margin:auto;padding:60px 25px;">
     <div style="width:100%;display:inline-block;margin:auto;text-align:center;">
       <img src="{{ asset('public/assets/img/v2/check.png') }}" alt="UrCorp" title="UrCorp" style="display:inline-block;width:128px;height:128px;margin-bottom:30px;" />
@@ -50,9 +50,11 @@
         </center>
       </div>
     </div>
-  <div style="width:80%;display:inline-block;margin:auto;min-height:110px;color: gray;padding: 20px 20px 20px 10%;border-radius:3px;font-family:helvetica,arial,sans-serif;font-size:16px;">
-    Este correo electrónico y cualquiera de sus anexos podrían contener información confidencial. Si usted no es el destinatario, por el presente se le notifica que cualquier difusión y copiado de este correo electrónico y cualquiera de sus anexos o uso de su contenido por cualquier medio está estrictamente prohibido. Si usted recibió este correo electrónico equivocadamente o por error, por favor notifíquelo al remitente inmediatamente y cancele este correo electrónico y todos sus anexos de su ordenador (computadora).
-  </div>
+    <div style="width:92%;display:block;margin: auto auto 25px auto;">
+      <div style="padding:15px;font-family:helvetica,arial,sans-serif;color:inherit;font-size:16px; text-align:justify">
+      Este correo electrónico y cualquiera de sus anexos podrían contener información confidencial. Si usted no es el destinatario, por el presente se le notifica que cualquier difusión y copiado de este correo electrónico y cualquiera de sus anexos o uso de su contenido por cualquier medio está estrictamente prohibido. Si usted recibió este correo electrónico equivocadamente o por error, por favor notifíquelo al remitente inmediatamente y cancele este correo electrónico y todos sus anexos de su ordenador (computadora).
+      </div>
+    </div>
   <div style="width:100%;display:inline-block;margin:auto;text-align:center;">
     <img src="{{ asset('public/assets/img/urcorp-gris.png') }}" alt="UrCorp" title="UrCorp" style="display:inline-block;width:300px;margin-bottom:30px;" />
   </div>
--- resources/views/site/emails/contest.blade.php
+++ resources/views/site/emails/contest.blade.php
@@ -1,26 +1,26 @@
 <?php
-  /*
+/*
   $contact['name']="Eduardo Vera - Web Developer";
   $contact['phone']="442·377·35·81";
   $contact['email']="eduardo.vera.pineda@gmail.com";
-  */
+*/
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8" />
-  <title>Solicitud de KIT</title>
+  <title>Concurso</title>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800,400italic,300italic' rel='stylesheet' type='text/css'>
 </head>
-<body style="font-family: 'Open sans',helvetica, arial, sans-serif;margin:0;background-color: #F5F5F5; color: #79888e">
-<div style="display:block;margin:auto;width:640px;background-color:#FFFFFF;">
+<body style="background-color: #F5F5F5;color: #79888e">
+<div style="display:block;width:80%;margin:auto;margin-top:50px;margin-bottom:15px;background-color:white;padding:15px;border-radius:3px;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);">
   <div style="width:92%;display:block;margin:auto;padding:60px 25px;">
     <div style="width:100%;display:inline-block;margin:auto;text-align:center;">
       <h1 style="font-weight:bold;margin-bottom:30px;">¡Se ha registrado un nuevo concursante!</h1>
     </div>
     <div style="width:100%;display:inline-block;margin:auto;font-weight:400;font-size:18px;text-align:center;">
-      <hr style="width: 50%; margin-top: 4%">
-      <div style="margin-top: 4%">
+      <hr style="width: 50%; margin-top: 2%">
+      <div style="margin-top: 2%">
         <table width="60%" style="margin:auto">
           <tr>
             <th style="border-bottom: 2px solid; text-align: left; color:#a7a6a6; font-weight:100">Nombre</th>
@@ -38,7 +38,7 @@
             </tr>
         </table>
       </div>
-      <hr style="width: 50%; margin-top: 4%">
+      <hr style="width: 50%; margin-top: 2%">
       <div style="text-align:left;padding-left:20%">
         <h4>Excelente ahora es momento de...</h4>
         <p>1.- Registrarlo en una base de datos análoga <br> 2.- Agregarlo a una lista de mailing
@@ -51,8 +51,11 @@
         </center>
       </div>
     </div>
-  <div style="width:80%;display:inline-block;margin:auto;min-height:110px;color: gray;padding: 20px 20px 20px 10%;border-radius:3px;font-family:helvetica,arial,sans-serif;font-size:16px;">
+
+  <div style="width:92%;display:block;margin: auto auto 25px auto;">
+    <div style="padding:15px;font-family:helvetica,arial,sans-serif;color:inherit;font-size:16px; text-align:justify">
     Este correo electrónico y cualquiera de sus anexos podrían contener información confidencial. Si usted no es el destinatario, por el presente se le notifica que cualquier difusión y copiado de este correo electrónico y cualquiera de sus anexos o uso de su contenido por cualquier medio está estrictamente prohibido. Si usted recibió este correo electrónico equivocadamente o por error, por favor notifíquelo al remitente inmediatamente y cancele este correo electrónico y todos sus anexos de su ordenador (computadora).
+    </div>
   </div>
   <div style="width:100%;display:inline-block;margin:auto;text-align:center;">
     <img src="{{ asset('public/assets/img/urcorp-gris.png') }}" alt="UrCorp" title="UrCorp" style="display:inline-block;width:300px;margin-bottom:30px;" />
